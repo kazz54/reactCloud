fa1255a084474866a6cc18628fe92e477575190a
kazz54 <jonathan.mrutu@gmail.com>
2021-08-27 22:54:41 +0300

    Update Banner.jsx

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -14,9 +14,9 @@ class Banner extends React.Component {
           <li><a href="#">Contact</a></li>
         </ul>
       </div>
-    </nav>
-   
-    
+    </nav> 
+    </Fragment>
+    <>
     <div className="hero">
       <div className="container">
         <h1>Welcome To My Website</h1>
@@ -31,7 +31,7 @@ class Banner extends React.Component {
       <h3>Content Two</h3>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur provident nostrum possimus inventore nisi laboriosam consequatur modi nulla eos, commodi, omnis distinctio! Maxime distinctio impedit provident, voluptates illo odio nostrum minima beatae similique a sint sapiente voluptatum atque optio illum est! Tenetur tempora doloremque quae iste aperiam hic cumque repellat?</p>
     </section>
-   </Fragment>
+   </>
     );
   }
 }
