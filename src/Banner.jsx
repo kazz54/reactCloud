0d5ef50759c655c2d8c222231e255518a1f0b43a
kazz54 <jonathan.mrutu@gmail.com>
2021-08-27 21:57:47 +0300

    Update Banner.jsx

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -3,14 +3,32 @@ import React from "react";
 class Banner extends React.Component {
   render() {
     return (
-      <section class="hero is-primary is-fullheight">
-        <div class="hero-body">
-          <div class="container has-text-centered">
-            <h1 class="title">Hello, Bulma!</h1>
-            <h2 class="subtitle">This is react app feat. Bulma</h2>
-          </div>
-        </div>
-      </section>
+      <nav className="nav">
+      <div className="container">
+        <h1 className="logo"><a href="/index.html">My Website</a></h1>
+        <ul>
+          <li><a href="#" class="current">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div className="hero">
+      <div className="container">
+        <h1>Welcome To My Website</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, consequuntur?</p>
+      </div>
+    </div>
+
+    <section className="container content">
+      <h2>Content One</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione dolorem voluptates eveniet tempora ut cupiditate magnam, sapiente, hic quo in ipsum iste soluta eaque perferendis nihil recusandae dolore officia aperiam corporis similique. Facilis quos tempore labore totam! Consectetur molestiae iusto ducimus error reiciendis aspernatur dolor, modi dolorem sit architecto, voluptate magni sunt unde est quas? Voluptates a dolorum voluptatum quo perferendis aut sit. Aspernatur libero laboriosam ab eligendi omnis delectus earum labore, placeat officiis sint illum rem voluptas ipsum repellendus iste eius recusandae quae excepturi facere, iure rerum sequi? Illum velit delectus dicta et iste dolorum obcaecati minus odio eligendi!</p>
+
+      <h3>Content Two</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur provident nostrum possimus inventore nisi laboriosam consequatur modi nulla eos, commodi, omnis distinctio! Maxime distinctio impedit provident, voluptates illo odio nostrum minima beatae similique a sint sapiente voluptatum atque optio illum est! Tenetur tempora doloremque quae iste aperiam hic cumque repellat?</p>
+    </section>
     );
   }
 }
