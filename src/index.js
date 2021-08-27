d14461f34c93a6f4ccead732e7611c96788ac94e
kazz54 <jonathan.mrutu@gmail.com>
2021-08-27 20:13:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import "bulma";
 
-import App from "./App";
+class Banner extends React.Component {
+  render() {
+    return (
+      <section class="hero is-primary is-fullheight">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title">Hello, Bulma!</h1>
+            <h2 class="subtitle">This is react app feat. Bulma</h2>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+export default Banner;
