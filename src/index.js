a8f1a9b543f62b117e1d1fa888cc3601a8d774a9
kazz54 <jonathan.mrutu@gmail.com>
2021-08-27 20:44:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import "bulma";
+import 'bulma/css/bulma.min.css';
 import Banner from "./Banner";
 
 ReactDOM.render(<Banner />, document.getElementById("root"));
