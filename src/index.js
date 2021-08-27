c7e99fc41d8b40541915620576fb294d7c23e141
kazz54 <jonathan.mrutu@gmail.com>
2021-08-27 20:18:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
+
 import React from "react";
+import ReactDOM from "react-dom";
 import "bulma";
+import Banner from "./component/Banner";
 
-class Banner extends React.Component {
-  render() {
-    return (
-      <section class="hero is-primary is-fullheight">
-        <div class="hero-body">
-          <div class="container has-text-centered">
-            <h1 class="title">Hello, Bulma!</h1>
-            <h2 class="subtitle">This is react app feat. Bulma</h2>
-          </div>
-        </div>
-      </section>
-    );
-  }
-}
-
-export default Banner;
+ReactDOM.render(<Banner />, document.getElementById
