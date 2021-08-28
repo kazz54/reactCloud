10f3a50856938ea22169bb32fc9f829a205c3929
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 20:46:30 +0300

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -3,14 +3,14 @@ import React from 'react';
 import useSticky from './useSticky.js';
 import Banner from './Banner';
 import Hero from './Hero';
-import About from './About';
+//import About from './About';
 function Home() {
     const { isSticky, element } = useSticky()
     return (
         <>
             <Banner sticky={isSticky} />
             <Hero element={element} />
-            <About/>
+            
         </>
     )
 }
