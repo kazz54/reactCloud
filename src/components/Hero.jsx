cd5a440cc3f017eab30676725d9098f99e466370
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 19:40:09 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,15 @@ const Hero = ({element}) => {
         <div className="hero">
         <div ref={element}>
       <div className="container">
-        
-        <About />
+        <h1>Welcome To My Website</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, consequuntur?</p>
       </div>
+     </div>
+      <section class="container content">
+        <About />
+      </section>
     </div>
-    </div>
+    
     )
 }
 
