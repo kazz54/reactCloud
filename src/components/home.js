93875ee1f2eecd09b6b590e5017196ca4a2dfb07
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 23:17:21 +0300

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import useSticky from './useSticky.js';
+//import useSticky from './useSticky.js';
 import Banner from './Banner';
 import Hero from './Hero';
 //import About from './About';
