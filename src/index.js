45d54110174a918aed6a1e0a2d776ebfa612ee8c
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 10:57:11 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+
+// COMPONENTS
+import Home from './components/home';
+
+const App = () =>{
+    return (
+        <BrowserRouter>
+            <div>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+             </Switch>   
+            </div>
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
