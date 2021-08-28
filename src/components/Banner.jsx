772562b8022c681279a5a68ca99b393865bd4e38
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 10:00:08 +0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,1 +1,22 @@
+import React from "react";
 
+class Banner extends React.Component {
+  render() {
+    return (
+      
+      <nav className="nav">
+      <div className="container">
+        <h1 className="logo"><a href="/index.html">My Website</a></h1>
+        <ul>
+          <li><a href="#" class="current">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav> 
+    );
+  }
+}
+
+export default Banner;
