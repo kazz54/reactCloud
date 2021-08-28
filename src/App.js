2b87fb42759c58f0b28d1f9dac46269d4034adf9
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 10:11:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import React, { Fragment } from 'react';
+import React, from 'react';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Banner from './components/Banner';
-import Home from './components/pages/Home';
+import Hero from './components/Hero';
 //import About from './components/pages/About';
 //import Register from './components/auth/Register';
 //import Login from './components/auth/Login';
@@ -15,26 +16,9 @@ import './App.css';
 
 const App = () => {
   return (
-    <AuthState>
-      <ContactState>
-        <AlertState>
-          <Router>
-            <Fragment>
-              <Navbar />
-              <div className='container'>
-                <Alerts />
-                <Switch>
-                  <PrivateRoute exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
-                </Switch>
-              </div>
-            </Fragment>
-          </Router>
-        </AlertState>
-      </ContactState>
-    </AuthState>
+    
+       <Banner />
+        <Hero />
   );
 };
 
