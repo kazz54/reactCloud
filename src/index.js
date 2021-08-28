c74b4bf3eda4abfb5ea76a2d3b7cc1e096699f08
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 09:51:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import React from "react";
-import ReactDOM from "react-dom";
-import Banner from "./Banner";
-import Hero from "./Hero";
-
-ReactDOM.render(<Banner />, <Hero />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
