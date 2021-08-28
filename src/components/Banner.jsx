fb9af6d14f5738d9ce790809739d4fbe39e1b64f
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 19:44:40 +0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ import React,{useEffect} from 'react';
 const Banner = ({ sticky }) => { 
 
   return (
-    <nav className={sticky ?"nav"}>
+    <nav className={sticky ?"nav :navbar"}>
       <div className="container">
         <h1 className="logo"><a href="/index.html">My Website</a></h1>
         <ul>
