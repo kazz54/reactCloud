724ba8a344266021139733a2a96aacfca1ccb42b
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 18:53:15 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import About from './About';
 
-const Hero = () =>{
+const Hero = ({element}) => {
     return(
         <div className="hero">
+        <div ref={element}>
       <div className="container">
         <h1>Welcome To My Website</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, consequuntur?</p>
