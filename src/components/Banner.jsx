cad97a48e4df0cbc44f0ab728fb85730ea8e56ac
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 20:26:26 +0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = ({ sticky }) => {
       <div className="container">
         <h1 className="logo"><a href="/index.html">My Website</a></h1>
         <ul>
-          <li><a href="#" class="current">Home</a></li>
+          <li><a href="#" className="current">Home</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">Contact</a></li>
