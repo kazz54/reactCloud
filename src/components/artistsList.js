f3ca8018b17916dc62c0bc76d24f838c637f71c7
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 10:51:45 +0300

    Create artistsList.js

---
+++ src/components/artistsList.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const ArtistsList = (props) =>{
+
+                
+                
+            return (
+  <div className="box content">
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
+  </div>
+)
+                
+            
+       }
+   
+
+export default ArtistsList;
