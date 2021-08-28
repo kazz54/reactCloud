d19a2f1df2d4885fa8f99a60173d28cd6f1e5ec3
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 23:10:22 +0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,10 +2,30 @@ import React,{useEffect} from 'react';
 //import './navbar.css';
 //import Logo from './../img/logo.svg';
 
-const Banner = ({ sticky }) => { 
+const Banner=() => {
+  const [scrolled,setScrolled]=React.useState(false);
+
+  const handleScroll=() => {
+    const offset=window.scrollY;
+    if(offset > 200 ){
+      setScrolled(true);
+    }
+    else{
+      setScrolled(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('scroll',handleScroll)
+  })
+
+  let x=['navbar'];
+  if(scrolled){
+    x.push('scrolled');
+  }
+
 
   return (
-    <nav className={sticky ? "nav navbar-sticky" : "nav"}>
+    <nav class="nav">
       <div className="container">
         <h1 className="logo"><a href="/index.html">My Website</a></h1>
         <ul>
