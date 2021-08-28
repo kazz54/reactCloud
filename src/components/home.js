327a91a93d4eab6ece62dcafb988dc280b056821
kazz54 <jonathan.mrutu@gmail.com>
2021-08-28 20:36:13 +0300

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import useSticky from './useSticky.js';
 import Banner from './Banner';
 import Hero from './Hero';
-
+import About from './About';
 function Home() {
     const { isSticky, element } = useSticky()
     return (
