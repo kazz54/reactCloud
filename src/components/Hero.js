8137489d0f98de990412b1d2eff5c9960a52ee91
kazz54 <jonathan.mrutu@gmail.com>
2021-08-30 14:03:05 +0300

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,1 +1,15 @@
+import React from 'react';
 import './hero.scss';
+
+const Hero = () =>{
+    return(
+        <div className="hero">
+      <div className="container">
+        <h1>Welcome To My Website</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, consequuntur?</p>
+      </div>
+    </div>
+    )
+}
+
+export default Hero;
