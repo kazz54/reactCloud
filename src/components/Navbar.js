3d44864d5eb0b5ff3a8962aa02c0a45f1658f7b6
kazz54 <jonathan.mrutu@gmail.com>
2021-08-30 10:22:57 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ const Navbar=() => {
           <img src={Logo} alt="Logo" title="Logo" />
        </div>
 
-        <nav className="navigation">
+        <nav className="navbar">
             <div className="container">
         <h1 className="logo"><a href="/index.html">My Website</a></h1>
         <ul>
