ee4cac6a00748f819e930f466f86473dc1950ac9
kazz54 <jonathan.mrutu@gmail.com>
2021-08-31 08:28:00 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,14 +28,14 @@ const Navbar=() => {
 
         <nav className="navigation">
             <div className="container">
-        <h1 className="logo"><a href="/index.html">My Website</a></h1>
+        
         <ul>
           <li><a href="#" className="current">Home</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
-      </div>
+      
     </nav>
     </header>
   )
