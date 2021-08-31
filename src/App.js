d421476665152c5193f6ac35c7c2a7c0b8aa5304
kazz54 <jonathan.mrutu@gmail.com>
2021-08-31 20:52:10 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Landing from './components/landing';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+      
+    </div>
+  );
+}
+
+export default App;
