de833d34f2ed77a27e765561c9322af68850098e
kazz54 <jonathan.mrutu@gmail.com>
2021-08-31 20:28:35 +0300

    Create landing.js

---
+++ src/components/landing.js
@@ -0,0 +1,63 @@
+import React,{ Fragment, Component } from 'react';
+import { createClient } from "contentful";
+//import HomeIterm from './homeIterm';
+import Banner from './Banner';
+import Hero from './Hero';
+//import Banner from './banner'
+import ArtistsList from './artistsList';
+
+// REQUESTS URL
+//const client = contentful.createClient({
+//  space: "9heip63yijn8",
+//  accessToken: "CQ4Ja0XdmfMso-gIfbIxBnEUBCFwIttPKT-R57fvZek" })
+
+//const URL_ARTIST = 'http://192.168.0.250:7000/artists'
+
+class Landing extends Component {
+//    constructor(props){
+//        super(props)
+
+//        this.state = {
+//            artists:''
+//        }
+//   }
+constructor() {
+    super();
+    this.state = { posts: [] };
+    this.client = createClient({
+      accessToken:
+        "CQ4Ja0XdmfMso-gIfbIxBnEUBCFwIttPKT-R57fvZek",
+      space: "9heip63yijn8"
+    });
+}
+  
+
+componentDidMount() {
+    this.fetchPosts().then(this.setPosts);
+  }
+fetchPosts = () => this.client.getEntries()
+
+setPosts = response => {
+    this.setState({
+      posts: response.items
+    })
+  }
+
+    render(){
+        return(
+            <Fragment>
+      <div className="container">
+      <Banner></Banner>
+      <Hero></Hero>
+      </container>
+      <section class="container content">
+      { this.state.posts.map(({fields}, i) =>
+        <ArtistsList key={i} {...fields} />
+      )}
+    </section>
+    </Fragment>
+        )
+    }
+}
+
+export default Landing;
