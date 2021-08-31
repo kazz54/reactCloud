b45a3cf0b1792321a643328cb66ed72bb67cf610
kazz54 <jonathan.mrutu@gmail.com>
2021-08-31 09:54:04 +0300

    Create Banner.jsx

---
+++ src/components/Banner.jsx
@@ -0,0 +1,44 @@
+import React,{useEffect} from 'react';
+//import './navbar.scss';
+//import Logo from './logo.svg';
+
+const Banner=() => {
+  const [scrolled,setScrolled]=React.useState(false);
+
+  const handleScroll=() => {
+    const offset=window.scrollY;
+    if(offset > 200 ){
+      setScrolled(true);
+    }
+    else{
+      setScrolled(false);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('scroll',handleScroll)
+  })
+
+  let x=['navbar'];
+  if(scrolled){
+    x.push('scrolled');
+  }
+  return (
+    <header className={x.join(" ")}>
+       
+
+        <nav className="navigation">
+            <div className="container">
+        
+        <ul>
+          <li><a href="#" className="current">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        </div>
+    </nav>
+    </header>
+  )
+};
+
+export default Banner;
