7f15705cd12528a0a717bca38273ceb55329eaf9
kazz54 <jonathan.mrutu@gmail.com>
2021-09-01 10:09:42 +0300

    Update landing.js

--- src/components/landing.js
+++ src/components/landing.js
@@ -65,7 +65,7 @@ setPosts = response => {
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi consequuntur magnam ipsum, commodi quam, non dolorem numquam veritatis, qui nam voluptas asperiores neque magni. Placeat, natus reprehenderit. Fugit, voluptatum commodi.</p>
   </div>
   <div className="footer"><p>Built with <3 by <a href="https://twitter.com/charliecodes">Charles Freeborn</a></p></div>
-</div>
+
       <section class="container content">
       { this.state.posts.map(({fields}, i) =>
         <ArtistsList key={i} {...fields} />
