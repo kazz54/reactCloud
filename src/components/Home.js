03767bcf665c0a7a61f3d3956b30a901630d5951
kazz54 <jonathan.mrutu@gmail.com>
2021-09-02 07:55:57 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -48,8 +48,8 @@ setPosts = response => {
         return(
             <Fragment>
        <Hero />
-      <section className="grid-1">
-        <div className="item-1">
+      
+        <div className="main">
 <p>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
 "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
 m</p>
@@ -60,7 +60,7 @@ m</p>
 "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
 m</p>
 </div>
-    </section>
+    
     <div className="main">
       { this.state.posts.map(({fields}, i) =>
         <ArtistsList key={i} {...fields} />
