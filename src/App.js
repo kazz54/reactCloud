3b6f1aaffe222db44626572a0d61750da5075c7a
kazz54 <jonathan.mrutu@gmail.com>
2021-09-04 14:37:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 //import Navbar from './components/Navbar';
 //import Hero from './components/Hero';
-import Home from './components/Home';
+//import Home from './components/Home';
 
 
 function App() {
