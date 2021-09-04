b058fe1afd8b24256f9647d172a0469c3218fb76
kazz54 <jonathan.mrutu@gmail.com>
2021-09-04 22:58:32 +0300

    Update Try.js

--- src/components/Try.js
+++ src/components/Try.js
@@ -1,1 +1,35 @@
 
+// REQUESTS URL
+const URL_ARTIST = 'http://192.168.0.250:7000/artists'
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            artists:''
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL_ARTIST, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+            this.setState({artists:json})
+        })
+    }
+
+
+    render(){
+        return(
+            <div>
+                <Banner></Banner>
+                <ArtistsList allArtists={this.state.artists}></ArtistsList>
+            </div>
+        )
+    }
+}
+
+export default Home;
