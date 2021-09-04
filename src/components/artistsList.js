951492565e98c5d1f93371c9a2fb42aa4362c9a3
kazz54 <jonathan.mrutu@gmail.com>
2021-09-04 16:18:52 +0300

    Create artistsList.js

---
+++ src/components/artistsList.js
@@ -0,0 +1,23 @@
+const Hero=() => {
+
+  const data=(
+  	<div className="hero">
+  	<h1>Welcome To My Website</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, consequuntur?</p>
+  	</div>
+  )
+
+  return (
+    <main className="container">
+	    
+	    {data}
+	    
+	    
+	    
+	    
+	    
+	   
+	    
+    </main>
+  )
+}
