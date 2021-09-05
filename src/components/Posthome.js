bec2e56cf127e0c6bd222309e13c7b32d5e136ef
kazz54 <jonathan.mrutu@gmail.com>
2021-09-05 21:21:28 +0300

    Update Posthome.js

--- src/components/Posthome.js
+++ src/components/Posthome.js
@@ -34,7 +34,7 @@ class Posthome extends Component {
         
    //     })
        
-   // }
+   }
 
 
     
@@ -44,6 +44,6 @@ class Posthome extends Component {
             </div>
         )
     
-}
+//}
 
 export default Posthome;
