60a8f8f6dcec5bcbde4b895ceb043b666fc0760d
kazz54 <jonathan.mrutu@gmail.com>
2021-09-05 20:45:00 +0300

    Update Poshome.js

--- src/components/Poshome.js
+++ src/components/Poshome.js
@@ -1,1 +1,40 @@
 
+import React,{ Component } from 'react';
+
+//import Banner from './banner'
+//import ArtistsList from './artistsList';
+
+// REQUESTS URL
+const URL_ARTIST = 'http://192.168.0.250:7000/artists'
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            artists:''
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL_ARTIST, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+            this.setState({artists:json})
+        })
+    }
+
+
+    render(){
+        return(
+            <div>
+                
+                <ArtistsList allArtists={this.state.artists}></ArtistsList>
+            </div>
+        )
+    }
+}
+
+export default Home;
