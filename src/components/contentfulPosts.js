a51cae7716b0d6a70d958090078e55cbcb30c35f
kazz54 <jonathan.mrutu@gmail.com>
2021-09-06 17:59:31 +0300

    Update contentfulPosts.js

--- src/components/contentfulPosts.js
+++ src/components/contentfulPosts.js
@@ -1,1 +1,18 @@
 
+import { createClient } from "contentful";
+const dotenv =  require('dotenv');
+dotenv.config()
+const space = process.env.REAT_PUBLIC_CONTENTFUL_SPACE_ID
+const accessToken = process.env.REAT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+const client = require('contentful').createClient({
+  space: space,
+  accessToken: accessToken,
+})
+
+export async function fetchEntries() {
+  const entries = await client.getEntries()
+  if (entries.items) return entries.items
+  console.log(`Error getting Entries for ${contentType.name}.`)
+}
+
+export default { fetchEntries }
