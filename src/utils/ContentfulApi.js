1f7471deaa28cb427aaa19adb3b0032391cfed56
kazz54 <jonathan.mrutu@gmail.com>
2021-09-06 15:19:43 +0300

    Update ContentfulApi.js

--- src/utils/ContentfulApi.js
+++ src/utils/ContentfulApi.js
@@ -1,6 +1,6 @@
 //const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
 //const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
-
+const accessToken = "CQ4Ja0XdmfMso-gIfbIxBnEUBCFwIttPKT-R57fvZek",
 const client = require('contentful').createClient({
   space: space,
   accessToken: accessToken,
