5882fb06cbdeafcc5220af35850f9580422f77e6
kazz54 <jonathan.mrutu@gmail.com>
2021-09-06 09:43:18 +0300

    Update artistsList.js

--- src/components/artistsList.js
+++ src/components/artistsList.js
@@ -1,26 +1,44 @@
 import React from 'react';
 //import { Link } from 'react-router-dom';
 import ReactDom from 'react-dom'
-import ReactMarkdown from 'react-
-import * as Markdown from 'react-markdown';
+import ReactMarkdown from 'react-markdown';
+//import * as Markdown from 'react-markdown';
 
 
-const ArtistsList = (props) =>{
+const ArtistsList=(props) => {
 
-                
-                
-            return (
-  <div className="box content">
-    <h1>{props.title}</h1>
-   <Markdown source={props.content} />
-  </div>
+  const markdown=(
+  	<div className="box content">
+  	<h1>{props.title}</h1>
+        <p>{props.content}</p>
+  	</div>
+  )
+  ReactDom.render(
+  <ReactMarkdown children={markdown} />,
+  document.body
 )
-                
-            
-       }
-   
+ 
+}
 
 export default ArtistsList;
+
+
+//const ArtistsList = (props) =>{
+
+                
+                
+//            return (
+//  <div className="box content">
+//    <h1>{props.title}</h1>
+//   <Markdown source={props.content} />
+//  </div>
+//)
+                
+            
+//       }
+   
+
+//export default ArtistsList;
 
 
 //import React from 'react';
