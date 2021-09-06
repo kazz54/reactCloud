3405d0ae2749bc7abb23aafb3ae551b5c64654c0
kazz54 <jonathan.mrutu@gmail.com>
2021-09-06 10:08:42 +0300

    Update artistsList.js

--- src/components/artistsList.js
+++ src/components/artistsList.js
@@ -1,44 +1,45 @@
 import React from 'react';
 //import { Link } from 'react-router-dom';
 import ReactDom from 'react-dom'
-import ReactMarkdown from 'react-markdown';
-//import * as Markdown from 'react-markdown';
+//import ReactMarkdown from 'react-markdown';
+import * as Markdown from 'react-markdown';
 
 
-const ArtistsList=(props) => {
+//const ArtistsList=(props) => {
 
-  const markdown=(
-  	<div className="box content">
-  	<h1>{props.title}</h1>
-        <p>{props.content}</p>
-  	</div>
-  )
-  ReactDom.render(
-  <ReactMarkdown source={markdown} />,
-  document.body
-)
- 
-}
-
-export default ArtistsList;
-
-
-//const ArtistsList = (props) =>{
-
-                
-                
-//            return (
-//  <div className="box content">
-//    <h1>{props.title}</h1>
-//   <Markdown source={props.content} />
-//  </div>
+//  const markdown=(
+//  	<div className="box content">
+//  	<h1>{props.title}</h1>
+//        <p>{props.content}</p>
+//  	</div>
+//  )
+//  ReactDom.render(
+//  <ReactMarkdown source={markdown} />,
+//  document.body
 //)
-                
-            
-//       }
-   
+ 
+//}
 
 //export default ArtistsList;
+
+
+const ArtistsList = (props) =>{
+
+                
+                
+            return (
+  <div className="box content">
+    <h1>{props.title}</h1>
+   <Markdown source={props.content} />,
+   document.body
+  </div>
+)
+                
+            
+      }
+   
+
+export default ArtistsList;
 
 
 //import React from 'react';
