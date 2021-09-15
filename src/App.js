f6441bb0fdff960f3c93d0b7f70dc861ef43402d
kazz54 <jonathan.mrutu@gmail.com>
2021-09-15 11:52:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 //import Navbar from './components/Navbar';
 //import Hero from './components/Hero';
-//import Home from './components/Home';
-import HomePost from './components/HomePost';
+import Home from './components/Home';
+//import HomePost from './components/HomePost';
 //import IndexPage from './components/IndexPage';
 
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       
-      <HomePost/>
+      <Home/>
     </div>
   );
 }
